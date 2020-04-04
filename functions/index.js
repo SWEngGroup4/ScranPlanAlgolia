5b3878622b574b198c6c1bb6d752d8243c7247c8
Nathan Billis <nathanbillis.nathan@gmail.com>
2020-04-04 11:02:13 +0100

    update function

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,27 @@
+// Initialize Algolia, requires installing Algolia dependencies:
+// https://www.algolia.com/doc/api-client/javascript/getting-started/#install
+//
+// App ID and API Key are stored in functions config variables
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// Authenticate to Algolia Database.
-// TODO: Make sure you configure the `algolia.app_id` and `algolia.api_key` Google Cloud environment variables.
+// const admin = require('firebase-admin');
 const algoliasearch = require('algoliasearch');
-const client = algoliasearch(functions.config().algolia.app_id, functions.config().algolia.api_key);
+// admin.initializeApp();
 
-// Name fo the algolia index for recipes content.
-const ALGOLIA_POSTS_INDEX_NAME = 'SCRANPLAN_RECIPES';
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 
-// Updates the search index when new recipe entries are created or updated.
-exports.indexentry = functions.database.ref('recipes').onWrite(
-    async (data, context) => {
-      const records = [];
-      const index = client.initIndex(ALGOLIA_POSTS_INDEX_NAME);
-      const childKey = data.id;
-      const firebaseObject = data.data();
+const ALGOLIA_INDEX_NAME_R = 'SCRANPLAN_RECIPES';
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 
-      firebaseObject.objectID = childKey;
-      records.push(firebaseObject);
+// Update the search index every time a post is written.
+exports.onNoteCreatedRecipes = functions.firestore.document('recipes/{noteId}').onCreate((snap, context) => {
+  // Get the note document
+  const note = snap.data();
 
-      await index.saveObject(records);
-    });
+  // Add an 'objectID' field which Algolia requires
+  note.objectID = context.params.noteId;
+
+  // Write to the algolia index
+  const index = client.initIndex(ALGOLIA_INDEX_NAME_R);
+  return index.saveObject(note);
+});
